d7c72d3cc9d9bdafb5a748e9c676dae44d7c5372
connieconnors <connors.connie@gmail.com>
2025-12-03 10:23:08 -0500

    Fix TSLogo import path

--- web/app/checkout/success/page.tsx
+++ web/app/checkout/success/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
-import { TSLogo } from "../../components/TSLogo";
+import { TSLogo } from "@/components/TSLogo";
 
 function SuccessContent() {
   const searchParams = useSearchParams();
