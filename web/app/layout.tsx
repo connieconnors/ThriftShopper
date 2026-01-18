74fee4f61fade051cebd51203452c812aef85ded
connieconnors <connors.connie@gmail.com>
2026-01-18 18:41:38 -0500

    style: darken PWA splash/status bar

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -34,7 +34,7 @@ export const metadata: Metadata = {
   manifest: "/manifest.json",
   appleWebApp: {
     capable: true,
-    statusBarStyle: "default",
+    statusBarStyle: "black-translucent",
     title: "ThriftShopper",
   },
   formatDetection: {
